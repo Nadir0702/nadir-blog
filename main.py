2520a44ade36a4e1e828e8c8269183288aeea776
Nadir Elmakias <nadir070299e@gmail.com>
2022-08-15 01:39:44 +0300

    Update db uri

--- main.py
+++ main.py
@@ -34,7 +34,10 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # #CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+db_uri = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+if db_uri.startswith("postgres://"):
+    db_uri = db_uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
